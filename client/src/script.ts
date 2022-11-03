90518d25be722685f2761900ba948fba7d86db42
toppair <tpatdv@gmail.com>
2022-11-03 10:52:28 +0100

    refactor: loader, marker

--- client/src/script.ts
+++ client/src/script.ts
@@ -1,68 +1,7 @@
 import { slidingWindows } from 'https://deno.land/std@0.159.0/collections/sliding_windows.ts';
 import morphdom from 'https://esm.sh/morphdom@2.6.1?no-dts';
 
-let markdownBody: HTMLElement;
 // const _log = Reflect.get(window, '_log');
-
-function setStyle(elem: HTMLElement, style: Record<string, string>) {
-  for (const [key, value] of Object.entries(style)) {
-    Reflect.set(elem.style, key, value);
-  }
-}
-
-function appendLoader() {
-  const loader = document.createElement('div');
-
-  setStyle(loader, {
-    position: 'absolute',
-    display: 'flex',
-    width: '60px',
-    top: '50%',
-    left: '50%',
-    justifyContent: 'space-between',
-    translate: '-50% -50%',
-  });
-
-  for (let i = 0; i < 3; i++) {
-    const dot = document.createElement('div');
-
-    setStyle(dot, {
-      width: '10px',
-      height: '10px',
-      borderRadius: '50%',
-      backgroundColor: 'blue',
-    });
-
-    loader.appendChild(dot);
-
-    dot.animate([
-      { scale: 0 },
-      { scale: 1 },
-      { scale: 0 },
-    ], {
-      duration: 1000,
-      iterations: Infinity,
-    });
-  }
-
-  markdownBody.appendChild(loader);
-}
-
-function appendMarker() {
-  const marker = document.createElement('div');
-
-  setStyle(marker, {
-    position: 'fixed',
-    width: '4px',
-    height: '4px',
-    top: '50%',
-    left: '5px',
-    borderRadius: '50%',
-    backgroundColor: 'blue',
-  });
-
-  document.body.appendChild(marker);
-}
 
 function setKeybinds() {
   document.addEventListener('keydown', (event: KeyboardEvent) => {
@@ -90,14 +29,12 @@ function setKeybinds() {
 }
 
 addEventListener('DOMContentLoaded', () => {
-  markdownBody = document.getElementById('markdown-body') as HTMLDivElement;
+  const markdownBody = document.getElementById('markdown-body') as HTMLDivElement;
   const base = document.getElementById('base') as HTMLBaseElement;
   const peek = Reflect.get(window, 'peek');
 
   markdownBody.classList.add(peek.theme);
 
-  appendLoader();
-  appendMarker();
   setKeybinds();
 
   let source: { lcount: number };
